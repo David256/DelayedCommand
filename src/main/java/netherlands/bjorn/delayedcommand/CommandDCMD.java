82ab352aeac5eefd755d0dba4a308762e186478d
David256 <wasterdavid@gmail.com>
2023-11-28 17:22:02 -0500

    add: an implementation of TabCompleter

--- src/main/java/netherlands/bjorn/delayedcommand/CommandDCMD.java
+++ src/main/java/netherlands/bjorn/delayedcommand/CommandDCMD.java
@@ -9,16 +9,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.scheduler.BukkitTask;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import static org.bukkit.Bukkit.getServer;
 
-public class CommandDCMD implements CommandExecutor {
+public class CommandDCMD implements CommandExecutor, TabCompleter {
+    public static String COMMAND_NAME = "dcmd";
 
     private final Main plugin;
 
@@ -70,5 +73,35 @@ public class CommandDCMD implements CommandExecutor {
 
         sender.sendMessage("Cancel this command with /dcmd cancel " + task.getTaskId());
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, String[] args) {
+        List<String> hints = new ArrayList<>();
+
+        if (args.length == 1) {
+            hints.add("once");
+            hints.add("repeat");
+            hints.add("infinite");
+            hints.add("cancel");
+        } else if (args.length == 2) {
+            if (args[0].equals("cancel")) {
+                hints.add("<id>");
+            } else if (args[0].equals("repeat")) {
+                hints.add("<times>");
+            } else {
+                hints.add("<seconds>");
+            }
+        } else if (args.length == 3) {
+            if (args[0].equals("repeat")) {
+                hints.add("<seconds>");
+            } else if (!args[0].equals("cancel")) {
+                hints.add("<commands>");
+            }
+        } else if (args.length == 4 && args[0].equals("repeat")) {
+            hints.add("<commands>");
+        }
+
+        return hints;
     }
 }
