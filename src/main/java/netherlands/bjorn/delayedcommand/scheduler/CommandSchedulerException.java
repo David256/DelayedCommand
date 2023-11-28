ce58dd7f83b4bbdd0730f4434c9a4fea6393fc7c
David256 <wasterdavid@gmail.com>
2023-11-28 16:21:58 -0500

    change: CommandSchedulerException can receive a list of string

--- src/main/java/netherlands/bjorn/delayedcommand/scheduler/CommandSchedulerException.java
+++ src/main/java/netherlands/bjorn/delayedcommand/scheduler/CommandSchedulerException.java
@@ -1,10 +1,17 @@
 package netherlands.bjorn.delayedcommand.scheduler;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public class CommandSchedulerException extends Exception {
     @Nullable
     private final String rawCmd;
+
+    public CommandSchedulerException(String reason, @Nullable List<String> rawCmds) {
+        super(reason);
+        this.rawCmd = rawCmds != null ? String.join(" ", rawCmds) : null;
+    }
+
     public CommandSchedulerException(String reason, @Nullable String rawCmd) {
         super(reason);
         this.rawCmd = rawCmd;
